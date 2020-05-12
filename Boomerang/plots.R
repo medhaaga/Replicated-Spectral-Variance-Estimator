07fdaed0f9116ce866e0084f0cc47979f5c0e1d3
medhaaga <64523609+medhaaga@users.noreply.github.com>
2020-05-12 13:21:12 +0530

    Phase-3 changes
    Running plots by averaging over 10 reps for each value of nsim.

--- Boomerang/plots.R
+++ Boomerang/plots.R
@@ -29,36 +29,39 @@ source("functions.R")
 
 ###############################################
 
-params <- matrix(c(1,2,7,1,8,9,1,9,9,2,6,7,2,8,7,2,9,7,2,10,8), nrow = 7, ncol=3, byrow = TRUE)
-m = 2
-check.pts <- c(1e3, 5e3, 1e4, 5e4, 7e4, 1e5)
-conv.pts <- seq(1e3, 1e5, 500)
+m = 5
+check.pts <- c(1e3, 2e3, 5e3, 1e4, 2e4)
+conv.pts <- seq(5e2, 1e5, 500)
 r <- length(check.pts)
 freq <- 1e3
 c.prob <- .95
-min <- 1e3
+min <- 5e2
 max <- 1e5
 
 #loop over all parameters A, B, C
-for (t in 1:7){
-  A <- params[t,1]
-  B <- params[t,2]
-  C <- params[t,3]
-  start <- matrix(c(2*C,1,1,2*C), 2, 2)
-  print(paste("For parameters A, B, C = ", A, B, C, " respectively."))
+#for (t in 1:7){
+  A <- 2
+  B <- 9
+  C <- 7
+  start <- matrix(0, nrow = m, ncol = 2)  #only depends on C
+  
+  for(i in 1:floor(m/2)){ 
+    start[i,] <- c(0, C*(2^(2-i)))
+    start[m-i+1,] <- c(C*(2^(2-i)), 0)
+  }
   
   ## 1.)Covergence plots
-  load(file = paste(paste("conv_data", min, max, A, B, C, sep = "_"), ".Rdata", sep = ""))
+  load(file = paste(paste("Out/", A, B, C, "/conv_data", m, min, max, A, B, C, sep = "_"), ".Rdata", sep = ""))
   
   #### 1a.) Sigma_11
-  pdf(file = paste(paste("conv_plot_Sig11", A, B, C, sep = "_"), ".pdf", sep = ""))
+  pdf(file = paste(paste("Out/", A, B, C, "/conv_plot_Sig11", m, A, B, C, sep = "_"), ".pdf", sep = ""))
   plot(conv.pts,asv.samp[1,1,], type = "l", col = "red", main = paste("Convergence plot of Sigma_11, A = ", A, ", B = ", B, ", C = ", C ), xlab = "Simulation size")
   lines(conv.pts, rsv.samp[1,1,], col="blue")
   legend("topright", legend=c("ASV", "RSV"),col=c("red", "blue"), lty=1, cex=1.2)
   dev.off()
   
   #### 1b.) Sigma_22
-  pdf(file = paste(paste("conv_plot_Sig22", A, B, C, sep = "_"), ".pdf", sep = ""))
+  pdf(file = paste(paste("Out/", A, B, C, "/conv_plot_Sig22", m, A, B, C, sep = "_"), ".pdf", sep = ""))
   plot(conv.pts,asv.samp[2,2,], type = "l", col = "red", main = paste("Convergence plot of Sigma_22, A = ", A, ", B = ", B, ", C = ", C ), xlab = "Simulation size")
   lines(conv.pts, rsv.samp[2,2,], col="blue")
   dev.off()
@@ -72,7 +75,7 @@ for (t in 1:7){
     det.asv[i] <- det(asv.samp[,,i])
   }
   
-  pdf(file = paste(paste("conv_plot_det", A, B, C, sep = "_"), ".pdf", sep = ""))
+  pdf(file = paste(paste("Out/", A, B, C, "/conv_plot_det", m, A, B, C, sep = "_"), ".pdf", sep = ""))
   plot(conv.pts,det.asv, type = "l", col="red", main = paste("Convergence plot of determinant, A = ", A, ", B = ", B, ", C = ", C ), xlab = "Simulation size")
   lines(conv.pts,det.rsv, col="blue")
   legend("topright", legend=c("ASV", "RSV"),col=c("red", "blue"), lty=1, cex=1.2)
@@ -84,27 +87,40 @@ for (t in 1:7){
     
     #### 2a.) Scatter plot of m Markov chains
     nsim <- check.pts[j]
-    chain1 <- markov.chain(A, B, C, nsim, start[1,])
-    chain2 <- markov.chain(A, B, C, nsim, start[2,])
+    chain <- array(0, dim = c(nsim,2,m))
     
+    for (p in 1:m){
+      chain[,,p] <- markov.chain(A, B, C, nsim, start[p,])
+    }
     
-    pdf(file = paste(paste("scatter_plot",nsim, A, B, C, sep = "_"), ".pdf", sep = ""))
-    plot(chain1[,1], chain1[,2], col = "red", xlim = c(-2,2*C), ylim = c(-2,2*C), main = paste("Scatter plot of two Markov chains, n = ", nsim, ", A = ", A, ", B = ", B, ", C = ", C), xlab = "X", ylab = "Y")
-    points(chain2[,1], chain2[,2], col = "green")
+    pdf(file = paste(paste("Out/", A, B, C, "/scatter_plot", m, nsim, A, B, C, sep = "_"), ".pdf", sep = ""))
+    plot(chain[,1,1], chain[,2,1], col = 2, xlim = c(-2,2*C), ylim = c(-2,2*C), main = paste("Scatter plot of m = ", m,  "Markov chains, n = ", nsim, ", A = ", A, ", B = ", B, ", C = ", C), xlab = "X", ylab = "Y")
+    for (p in 2:m){
+      points(chain[,1,p], chain[,2,p], col = p+1)
+    }
+    
+    dev.off()
+    
+    pdf(file = paste(paste("Out/", A, B, C, "/trace", m, nsim, A, B, C, sep = "_"), ".pdf", sep = ""))
+    par(mfrow = c(m,2))
+    for(p in 1:m){
+      plot.ts(chain[,1,p], main = paste("x component of chain -", p))
+      plot.ts(chain[,2,p], main = paste("y component of chain -", p))
+    }
     dev.off()
     
     #Comparitative density plots of ASV and RSV
-    load(file = paste(paste("out",nsim,A,B,C, sep = "_"),".Rdata", sep = ""))
+    load(file = paste(paste("Out/", A, B, C, "/out", m, nsim, A, B, C, sep = "_"), ".Rdata", sep = ""))
     
     #### 2b.) Sigma_11
-    pdf(file = paste(paste("density_S11",nsim, A, B, C, sep = "_"), ".pdf", sep = ""))
+    pdf(file = paste(paste("Out/", A, B, C, "/density_S11",m, nsim, A, B, C, sep = "_"), ".pdf", sep = ""))
     plot(density(asv.samp[1,1,]), col="red", main = paste("Density plot for Sigma_11, nsim = ", nsim, ", A = ", A, ", B = ", B, ", C = ", C))
     lines(density(rsv.samp[1,1,]), col="blue")
     legend("topright", legend=c("ASV", "RSV"),col=c("red", "blue"), lty=1, cex=1.2)
     dev.off()
     
     #### 2c.) Sigma_22
-    pdf(file = paste(paste("density_S22",nsim, A, B, C, sep = "_"), ".pdf", sep = ""))
+    pdf(file = paste(paste("Out/", A, B, C, "/density_S22", m, nsim, A, B, C, sep = "_"), ".pdf", sep = ""))
     plot(density(asv.samp[2,2,]), col="red", main = paste("Density plot for Sigma_22, nsim = ", nsim, ", A = ", A, ", B = ", B, ", C = ", C))
     lines(density(rsv.samp[2,2,]), col="blue")
     legend("topright", legend=c("ASV", "RSV"),col=c("red", "blue"), lty=1, cex=1.2)
@@ -119,14 +135,14 @@ for (t in 1:7){
       det.asv[j] <- det(asv.samp[,,j])
     }
     
-    pdf(file = paste(paste("density_det",nsim, A, B, C, sep = "_"), ".pdf", sep = ""))
+    pdf(file = paste(paste("Out/", A, B, C, "/density_det", m, nsim, A, B, C, sep = "_"), ".pdf", sep = ""))
     plot(density(det.asv), col="red", main = paste("Density plot for determinant, nsim = ", nsim, ", A = ", A, ", B = ", B, ", C = ", C))
     lines(density(det.rsv), col="blue")
     legend("topright", legend=c("ASV", "RSV"),col=c("red", "blue"), lty=1, cex=1.2)
     dev.off()
     
-    print(paste("Coverage probabilities for nsim =  ", nsim, ", A = ", A, ", B = ", B, ", C = ", C, " are: "))
+    print(paste("Coverage probabilities for nsim =  ", nsim, ", m = ", m, ", A = ", A, ", B = ", B, ", C = ", C, " are: "))
     print(paste("ASV : ", mean(asv.coverage), "RSV: ", mean(rsv.coverage)))
   }
-}
+#}
 
--- Boomerang/simulations.R
+++ Boomerang/simulations.R
@@ -13,7 +13,13 @@ sourceCpp("lag.cpp")
 
 create.output <- function(A,B,C,m,check.pts,freq,c.prob){
   
-  start <- matrix(c(2*C,1,1,2*C), 2, 2)  #only depends on C
+  start <- matrix(0, nrow = m, ncol = 2)  #only depends on C
+  
+  for(i in 1:floor(m/2)){ 
+    start[i,] <- c(0, C*(2^(2-i)))
+    start[m-i+1,] <- c(C*(2^(2-i)), 0)
+  }
+  
   critical <- qchisq(c.prob, df=2)
   T.mean <- true.mean(A,B,C)
   
@@ -56,7 +62,7 @@ create.output <- function(A,B,C,m,check.pts,freq,c.prob){
       if (chi.sq.asv <= critical) {asv.coverage[j]=1}
       if (chi.sq.rsv <= critical) {rsv.coverage[j]=1}
     }
-    save(asv.samp,rsv.samp,asv.coverage,rsv.coverage, file = paste(paste("Out/out",nsim,A,B,C, sep = "_"),".Rdata", sep = ""))
+    save(asv.samp,rsv.samp,asv.coverage,rsv.coverage, file = paste(paste("Out/", A, B, C, "/out", m,nsim,A,B,C, sep = "_"),".Rdata", sep = ""))
   }
   
 }
@@ -64,69 +70,86 @@ create.output <- function(A,B,C,m,check.pts,freq,c.prob){
 ##################################################
 #function for storing convergence plots data
 ##################################################
-convergence <- function(min, max, A, B, C){
+convergence <- function(min, max, A, B, C, m, rep=100){
 
-  start <- matrix(c(2*C,1,1,2*C), 2, 2)
-  master.chain <- array(0,dim = c(max,2,m))
+  start <- matrix(0, nrow = m, ncol = 2)  #only depends on C
   
-  for (k in 1:m){
-    master.chain[,,k] <- markov.chain(A,B,C,max,start[k,])
+  for(i in 1:floor(m/2)){ 
+    start[i,] <- c(0, C*(2^(2-i)))
+    start[m-i+1,] <- c(C*(2^(2-i)), 0)
   }
-  conv.pts <- seq(min, max,500)
   
+  master.chain.rep <- array(0,dim = c(max, 2, m, rep))
+  
+  for (r in 1:rep){
+    for (k in 1:m){
+      master.chain.rep[,,k,r] <- markov.chain(A, B, C, max, start[k,])
+    }
+  }
+  
+  conv.pts <- seq(min, max,500)
   l <- length(conv.pts)
   asv.samp <- array(0, dim = c(2,2,l))
   rsv.samp <- array(0, dim = c(2,2,l))
   
   for (j in 1:l){
     nsim = conv.pts[j]
-    chain <- master.chain[1:nsim,,]
-    sve <- array(0, dim = c(2,2,m))
-    rsve <- array(0, dim = c(2,2,m))
-    b <- rep(0,m)
-    
-    for (k in 1:m){
-      b[k] <- batchSize(chain[,,k], method = "bartlett")
+    sve.rep <- array(0, dim = c(2, 2, rep))
+    rsve.rep <- array(0, dim = c(2, 2, rep))
+    for (r in 1:rep){
+      chain <- master.chain.rep[1:nsim,,,r]
+      sve <- array(0, dim = c(2,2,m))
+      rsve <- array(0, dim = c(2,2,m))
+      b <- rep(0,m)
+      
+      for (k in 1:m){
+        b[k] <- batchSize(chain[,,k], method = "bartlett")
+      }
+      
+      b.avg <- mean(b)
+      global.mean <- apply(chain,2,mean)
+      
+      for (k in 1:m){
+        chain.cen.loc <- scale(chain[,,k], scale = FALSE)  ## X_st - bar(X)_s
+        sve[,,k] <- mSVEfft(A = chain.cen.loc, b = b.avg)
+        chain.cen <- scale(chain[,,k], center = global.mean, scale =FALSE)
+        rsve[,,k] <- mSVEfft(A = chain.cen, b = b.avg)
+        
+      }
+      
+      sve.rep[,,r] <- apply(sve, c(1,2), mean)
+      rsve.rep[,,r] <- apply(rsve, c(1,2), mean)
     }
     
-    b.avg <- mean(b)
-    global.mean <- apply(chain,2,mean)
-    
-    for (k in 1:m){
-      chain.cen.loc <- scale(chain[,,k], scale = FALSE)  ## X_st - bar(X)_s
-      sve[,,k] <- mSVEfft(A = chain.cen.loc, b = b.avg)
-      chain.cen <- scale(chain[,,k], center = global.mean, scale =FALSE)
-      rsve[,,k] <- mSVEfft(A = chain.cen, b = b.avg)
-    }
-    
-    asv.samp[,,j] <- apply(sve, c(1,2), mean)
-    rsv.samp[,,j] <- apply(rsve, c(1,2), mean)
-    if(j %% l/10 == 0) print(paste("Percentage completion: ", round(100*j/l, 2)))
+    asv.samp[,,j] <- apply(sve.rep, c(1,2), mean)
+    rsv.samp[,,j] <- apply(rsve.rep, c(1,2), mean)
+    print(paste("Percentage completion: ", round(100*j/l, 2)))
   }
-  save(asv.samp,rsv.samp, file = paste(paste("Out/conv_data", min, max, A, B, C, sep = "_"), ".Rdata", sep = ""))
+  save(asv.samp,rsv.samp, file = paste(paste("Out/", A, B, C, "/conv_data", m, min, max, A, B, C, sep = "_"), ".Rdata", sep = ""))
   
 }
 
 
 ######################################################################
-#10*3 matrix for A,B,C parameters. Each row corresponds to a different suitable parameterization
-params <- matrix(c(1,2,7,1,8,9,1,9,9,2,6,7,2,8,7,2,9,7,2,10,8), nrow = 7, ncol=3, byrow = TRUE)
-m = 2
+
+A <- 2
+B <- 9
+C <- 7
+m = 5
+
 #sims for plotting densities and calculating coverage
 
 check.pts <- c(1e3, 2e3, 5e3, 1e4, 2e4)
 freq <- 1e3  #100 for now, will change later
+rep <- 10
 c.prob <- .95
 min <- 5e2
-max <- 1e4
+max <- 1e5
+conv.pts <- seq(min, max, 500)
 
-t <- 6 ## choosing (2,9,7)
-# for creating .Rdata files for each set of parameter values
-# for (t in 1:7)
-# {
-  # print(paste("Sampling for A, B, C, = ", params[t,1], params[t,2], params[t,3], "respectively", sep = " "))
-  print("Carrying out 1000 repititions for each value of nsim in check.pts")
-  create.output(params[t,1], params[t,2], params[t,3], m, check.pts, freq, c.prob)
-  print("Carrying out simulations for convergence plots of ASV and RSV in the range(1e3, 1e5")
-  convergence(min, max, params[t,1], params[t,2], params[t,3])
-# }
+print("Carrying out 1000 repititions for each value of nsim in check.pts")
+create.output(A, B, C, m, check.pts, freq, c.prob)
+
+print("Carrying out simulations for convergence plots of ASV and RSV in the range(1e3, 1e5")
+convergence(min, max, A, B, C, m, rep)
+
