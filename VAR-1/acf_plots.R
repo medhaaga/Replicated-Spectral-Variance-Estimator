acea75e0e0e19eff6d8f5a28b871b761a935c518
Dootika Vats <dootika1990@gmail.com>
2020-05-25 16:41:34 +0530

    Magical, poofy, acf plots, for VAR

--- VAR-1/acf_plots.R
+++ VAR-1/acf_plots.R
@@ -4,8 +4,8 @@ library(rep.acf.ccf)
 
 
 m <- 2
-nsim <- 1000
-p <- 5
+nsim <- 1e3
+p <- 2
 omega <- diag(p)
 for (i in 1:(p-1)){
   for (j in 1:(p-i)){
@@ -14,8 +14,14 @@ for (i in 1:(p-1)){
   }
 }
 
+p <- 2
+phi <- diag(c(.99, .01))
+dummy <- matrix(1:p^2, nrow = p, ncol = p)
+dummy <- qr.Q(qr(dummy))
+phi <- dummy %*% phi %*% t(dummy)
 
-phi <- diag(c(.99, .1, .1, .1, .1))
+# phi <- phi/(max(eigen(phi)$values) + .1)
+
 sigma <- target.sigma(phi, omega)
 target <- target.sigma(phi, omega)
 truth <- true.sigma(phi, var = target)
@@ -31,8 +37,8 @@ for (i in 1:lag.max){
 start <- matrix(0, nrow = m, ncol = p)  #only depends on C
 
 for(i in floor(m/2):1){
-  start[i,] <- 2*i*sqrt(diag(sigma))
-  start[m-i+1,] <- -2*i*sqrt(diag(sigma))
+  start[i,] <- 1*i*sqrt(diag(sigma))
+  start[m-i+1,] <- -1*i*sqrt(diag(sigma))
 }
 
 mc.chain.list <- list()
@@ -43,24 +49,29 @@ for (i in 1:m){
   mc.chain.list[[i]] = chain
 }
 global.mean <- global.mean/m
+plot(mc.chain.list[[1]], xlim = range(c(mc.chain.list[[1]][,1],mc.chain.list[[2]][,1]) ), ylim = range(c(mc.chain.list[[1]][,2],mc.chain.list[[2]][,2]) ), asp = 1)
+points(mc.chain.list[[2]], col = "red")
+
+plot.ts(mc.chain.list[[1]])
+plot.ts(mc.chain.list[[2]])
 
 acf.list <- combined_acf(mc.chain.list, chain = 1, component = seq(1,p), lag.max = 150, type = "covariance")
-ccf.list <- combined_ccf(mc.chain.list, chain = 1, component = c(1,5), lag.max = 150, type = "covariance")
+ccf.list <- combined_ccf(mc.chain.list, chain = 1, component = c(1,2), lag.max = 150, type = "covariance")
 
-pdf(file = paste("Out/acf, n = ", nsim, ".pdf", sep = ""), title = paste("Locally vs. globally centered ACF plot, nsim = ", nsim))
+# pdf(file = paste("Out/acf, n = ", nsim, ".pdf", sep = ""), title = paste("Locally vs. globally centered ACF plot, nsim = ", nsim))
 par(mfrow = c(3,2))
 
 plot(acf.list[[1]][[1]], main = paste("Locally centered ACF plot for component - 1"), xlim = range(0, lag.max), ylim = c(min(min(acf.list[[1]][[1]]$acf), min(true.acf[1,1,])), max(max(acf.list[[1]][[1]]$acf), max(true.acf[1,1,]))))
 lines(seq(-lag.max, lag.max), true.acf[1,1,], col = "red")
 plot(acf.list[[1]][[2]], main = paste("Globally centered ACF plot for component - 1"), xlim = range(0, lag.max), ylim = c(min(min(acf.list[[1]][[2]]$acf), min(true.acf[1,1,])), max(max(acf.list[[1]][[2]]$acf), max(true.acf[1,1,]))))
 lines(seq(-lag.max, lag.max), true.acf[1,1,], col = "red")
-plot(acf.list[[5]][[1]], main = paste("Locally centered ACF plot for component - 5"), xlim = range(0, lag.max), ylim = c(min(min(acf.list[[5]][[1]]$acf), min(true.acf[5,5,])), max(max(acf.list[[5]][[1]]$acf), max(true.acf[5,5,]))))
-lines(seq(-lag.max, lag.max), true.acf[5,5,], col = "red")
-plot(acf.list[[5]][[2]], main = paste("Globally centered ACF plot for component - 5"), xlim = range(0, lag.max), ylim = c(min(min(acf.list[[5]][[2]]$acf), min(true.acf[5,5,])), max(max(acf.list[[5]][[2]]$acf), max(true.acf[5,5,]))))
-lines(seq(-lag.max, lag.max), true.acf[5,5,], col = "red")
+plot(acf.list[[2]][[1]], main = paste("Locally centered ACF plot for component - 5"), xlim = range(0, lag.max), ylim = c(min(min(acf.list[[2]][[1]]$acf), min(true.acf[2,2,])), max(max(acf.list[[2]][[1]]$acf), max(true.acf[2,2,]))))
+lines(seq(-lag.max, lag.max), true.acf[2,2,], col = "red")
+plot(acf.list[[2]][[2]], main = paste("Globally centered ACF plot for component - 5"), xlim = range(0, lag.max), ylim = c(min(min(acf.list[[2]][[2]]$acf), min(true.acf[2,2,])), max(max(acf.list[[2]][[2]]$acf), max(true.acf[2,2,]))))
+lines(seq(-lag.max, lag.max), true.acf[2,2,], col = "red")
 
 plot(ccf.list[[1]], main = "Locally centered CCF plot between 1 and 5", xlim = c(-lag.max, lag.max), ylim = range(ccf.list[[1]]$acf))
-lines(seq(-lag.max, lag.max), true.acf[1,5,], col = "red")
+lines(seq(-lag.max, lag.max), true.acf[1,2,], col = "red")
 plot(ccf.list[[2]], main = "Globally centered CCF plot between 1 and 5", ylim = range(ccf.list[[2]]$acf))
-lines(seq(-lag.max, lag.max), true.acf[1,5,], col = "red")
-dev.off()
+lines(seq(-lag.max, lag.max), true.acf[1,2,], col = "red")
+# dev.off()
